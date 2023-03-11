c6ac9b938a56b19a3834c5776c6af37141cb7991
Andrii Yerko <aerko12@gmail.com>
2023-03-11 22:14:08 +0200

    Use shortest path alghorithm for moving
    https://www.kaggle.com/code/jtbontinck/shortest-path-on-mars-using-networkx

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Dict
 import sys
 from argparse import Namespace
 
-from agent import Agent
+from bot import Agent
 from lux.config import EnvConfig
 from lux.kit import GameState, process_obs, to_json, from_json, process_action, obs_to_game_state
 ### DO NOT REMOVE THE FOLLOWING CODE ###
